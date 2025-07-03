2546e42adc1356d21eec3a380052b4cd640073b9
Foxdemon194 <109841490+Foxdemon194@users.noreply.github.com>
2025-07-03 15:05:08 -0400

    Worked on event listeners for the interact button, but it is incomplete

--- Assets/F_Input/CS_GameInput.cs
+++ Assets/F_Input/CS_GameInput.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CS_GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     private IA_Player iAPlayer;
 
     private void Awake()
     {
         iAPlayer = new IA_Player();
         iAPlayer.Player.Enable();
+
+        iAPlayer.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized()
@@ -17,7 +26,6 @@ public class CS_GameInput : MonoBehaviour
         Vector2 inputVector = iAPlayer.Player.Move.ReadValue<Vector2>();
 
         inputVector = inputVector.normalized;
-        //Debug.Log(inputVector);
         return inputVector;
     }
 }
