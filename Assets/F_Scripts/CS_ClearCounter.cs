8129eb776af97a313bfe2a21d6d23128a64ee522
Foxdemon194 <109841490+Foxdemon194@users.noreply.github.com>
2025-07-08 12:13:24 -0400

    Worked on the ingredient swap mechanic, still incomplete

--- Assets/F_Scripts/CS_ClearCounter.cs
+++ Assets/F_Scripts/CS_ClearCounter.cs
@@ -7,18 +7,42 @@ public class CS_ClearCounter : MonoBehaviour
 
     [SerializeField] SO_KitchenObject sOKitchenObject;
     [SerializeField] Transform CounterTopPoint;
+    [SerializeField] CS_ClearCounter secondClearCounter;
+    [SerializeField] bool testing;
 
 
     private CS_KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
 
     public void Interact()
     {
         if (kitchenObject == null)
         {        
-        Transform kitchenObjectTransform = Instantiate(sOKitchenObject.prefab, CounterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+            Transform kitchenObjectTransform = Instantiate(sOKitchenObject.prefab, CounterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
 
-        kitchenObject = kitchenObjectTransform.GetComponent<CS_KitchenObject>();
+            kitchenObject = kitchenObjectTransform.GetComponent<CS_KitchenObject>();
+            kitchenObject.SetClearCounter(this);
         }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return CounterTopPoint;
     }
 }
--- Assets/F_Scripts/CS_KitchenObject.cs
+++ Assets/F_Scripts/CS_KitchenObject.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 public class CS_KitchenObject : MonoBehaviour
 {
     [SerializeField] private SO_KitchenObject sOKitchenObject;
+
+    private CS_ClearCounter clearCounter;
+
     public SO_KitchenObject GetSO_KitchenObject()
     {
         return sOKitchenObject;
+    }
+
+    public void SetClearCounter(CS_ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public CS_ClearCounter GetClearCounter() 
+    {
+        return clearCounter;
     }
 }
