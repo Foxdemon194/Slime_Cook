37b9a2b1de69988b17c1df0b39ebebd26cab5d65
Foxdemon194 <109841490+Foxdemon194@users.noreply.github.com>
2025-09-23 14:07:04 -0400

    plate now matches burger progression visuals

--- Assets/F_Scripts/CS_PlateCompleteVisual.cs
+++ Assets/F_Scripts/CS_PlateCompleteVisual.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CS_PlateCompleteVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
     {
-        
+        public SO_KitchenObject kitchenObjectSO;
+        public GameObject gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private CS_PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList;
+
+    private void Start()
     {
-        
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            kitchenObjectSOGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, CS_PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            if (kitchenObjectSOGameObject.kitchenObjectSO == e.kitchenObjectSO)
+            {
+                kitchenObjectSOGameObject.gameObject.SetActive(true);
+            }
+        }
     }
 }
--- Assets/F_Scripts/CS_PlateKitchenObject.cs
+++ Assets/F_Scripts/CS_PlateKitchenObject.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CS_PlateKitchenObject : CS_KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs: EventArgs
+    {
+        public SO_KitchenObject kitchenObjectSO;
+    }
+
     [SerializeField] private List<SO_KitchenObject> validKitchenObjectSOList;
     private List<SO_KitchenObject> kitchenObjectSOList;
 
@@ -28,6 +36,11 @@ public class CS_PlateKitchenObject : CS_KitchenObject
         else
         {
             kitchenObjectSOList.Add(sO_KitchenObject);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+            {
+                kitchenObjectSO = sO_KitchenObject
+            });
             return true;
         }
     }
